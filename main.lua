162cc698ebc35098c934b3b22862edd6bbd477e1
jardimdanificado <jardeltorreslima01@gmail.com>
2023-08-26 04:23:50 -0300

    mocegui updated to raylib + added some fonts

--- main.lua
+++ main.lua
@@ -1,52 +1,33 @@
 local mocegui = require "mocegui"
 local util = require "mocegui.luatils"
-local pending={}
 
-function love.load()
+function main()
 	mocegui.load()
-  local counter = 0
-  local defwin = mocegui.newWindow(nil,{(love.graphics.getWidth()/2)-144,48},{288,94}) -- default window
-  defwin.text.new('Right mouse button close windows.\nMiddle mouse button move windows.\nLeft mouse button interacts, and also move\nwindows if click on title bar.\nWhen a window got title bar it also got a close\nbutton on top12-right',{4,4})
-  local windowspawner = mocegui.newWindow('window spawner',{(love.graphics.getWidth()/2)-50,148},{100,60},{0.3,0.4,0.5,1}) -- default window
-  windowspawner.button.new({windowspawner.size[1]/2-16,windowspawner.size[2]/2-4},{32,16},function ()
-  	counter = counter + 1
-  	mocegui.newWindow("window " .. counter,{mocegui.util.random(0,love.graphics.getWidth()/2),mocegui.util.random(0,love.graphics.getHeight()/2)-16},{mocegui.util.random(0,love.graphics.getWidth()/2)+16,mocegui.util.random(0,love.graphics.getHeight()/2)+16})
-  end)
-  
-  util.agendar(pending, function(obj)
-    for i,v in ipairs(mocegui.window) do
-       if v == obj then
-         mocegui.window[i] = nil
-         util.array.selfclear(mocegui.window)
-         break
-       end
+    mocegui.font[1] = rl.LoadFontEx("data/font/Cascadia.ttf", 12, nil, 0)
+
+    local counter = 0
+    local defwin = mocegui.newWindow(nil,{x=(600/2)-144,y=48},{x=288,y=94}) -- default window
+    defwin.text.new('Right mouse button close windows.\nMiddle mouse button move windows.\nLeft mouse button interacts, and also move\nwindows if click on title bar.\nWhen a window got title bar it also got a close\nbutton on top12-right',{x=4,y=4})
+    local windowspawner = mocegui.newWindow('window spawner',{x=(600/2)-50,y=148},{x=100,y=60},{r=120,g=100,b=128,a=255}) -- default window
+    windowspawner.button.new({x=windowspawner.size.x/2-16,y=windowspawner.size.y/2-4},{x=32,y=16},function ()
+        counter = counter + 1
+        mocegui.newWindow("window " .. counter,{x=mocegui.util.random(0,600/2),y=mocegui.util.random(0,400/2)-16},{x=mocegui.util.random(0,600/2)+16,y=mocegui.util.random(0,400/2)+16})
+    end)
+    util.agendar(mocegui.pending, function(obj)
+        for i,v in ipairs(mocegui.window) do
+            if v == obj then
+                mocegui.window[i] = nil
+                mocegui.window = util.array.clear(mocegui.window)
+                break
+            end
+        end
+    end,{defwin},4)
+    while not rl.WindowShouldClose() do
+        mocegui.render()
     end
-  end,{defwin},4)
+    mocegui.close()
 end
 
-function love.keypressed(key)
-	mocegui.keypressed(key)
-end
-
-function love.mousemoved( x, y, dx, dy, istouch )
-	mocegui.mousemoved( x, y, dx, dy, istouch )
-end
-
-function love.mousereleased(x, y, button)
-	mocegui.mousereleased(x,y,button)
-end
-
-function love.mousepressed(x, y, button, istouch)
-	mocegui.mousepressed(x, y, button, istouch)
-end
-
-function love.update()
-  mocegui.util.repeater(pending)
-	mocegui.update()
-end
-
-function love.draw()
-	mocegui.draw()
-end
+main()
 
 return mocegui
